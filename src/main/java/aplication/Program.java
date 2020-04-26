ff6fd5eab05777b51c8c312c7a65086b70daa340
Tomas Herculano <tomasherculano@gmail.com>
2020-04-26 15:50:09 -0300

    Recebe o arquivo logCurrent.txt do Cliente via Socket

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,9 +1,13 @@
 package aplication;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Program {
 
@@ -16,12 +20,31 @@ public class Program {
 			Socket cliente = servidor.accept();
 			System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
-			Scanner s = new Scanner(cliente.getInputStream());
+			InputStream in = cliente.getInputStream();
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader reader = new BufferedReader(isr);
+			String fName = reader.readLine();
+			System.out.println(fName);
+			File f1 = new File("logs\\" + fName);
+			FileOutputStream out = new FileOutputStream(f1);
+
+			int tamanho = 4096; // buffer de 4KB
+			byte[] buffer = new byte[tamanho];
+			int lidos = -1;
+			while ((lidos = in.read(buffer, 0, tamanho)) != -1) {
+				System.out.println(lidos);
+				out.write(buffer, 0, lidos);
+			}
+			out.flush();
+			out.close();
+
+			
+			/*Scanner s = new Scanner(cliente.getInputStream());
 			while (s.hasNextLine()) {
 				System.out.println(s.nextLine());
 			}
 
-			s.close();
+			s.close();*/
 			servidor.close();
 			cliente.close();
 		} catch (IOException e) {
